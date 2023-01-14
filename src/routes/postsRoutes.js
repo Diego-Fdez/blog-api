df9361851e3d78d8bcfaddbc0a08b68253c529d6
Diego-Fdez <dieghoxcr7@gmail.com>
2023-01-13 18:37:21 -0600

    add express-validator to the routes

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import { body } from 'express-validator';
+import apicache from 'apicache';
 import {
   addPost,
   getPost,
@@ -8,11 +10,33 @@ import {
 } from '../controllers/postsController.js';
 
 const router = express.Router();
+const cache = apicache.middleware;
 
-router.get('/', getPosts);
-router.get('/:id', getPost);
-router.post('/', addPost);
-router.put('/:id', updatePost);
+router.get('/:offSet', cache('2 minutes'), getPosts);
+router.get('/post/:id', getPost);
+router.post(
+  '/',
+  body('title', 'Post title is required').not().isEmpty(),
+  body('body', 'The body of the post is required').not().isEmpty(),
+  body('category', 'Category is required').not().isEmpty(),
+  body('createdBy', 'You must enter the name of the user who creates the post')
+    .not()
+    .isEmpty(),
+  addPost
+);
+router.put(
+  '/:id',
+  body('title', 'Post title is required').not().isEmpty(),
+  body('body', 'The body of the post is required').not().isEmpty(),
+  body('category', 'Category is required').not().isEmpty(),
+  body('createdBy', 'You must enter the name of the user who creates the post')
+    .not()
+    .isEmpty(),
+  body('EditedBy', 'You must enter the name of the user who edit the post')
+    .not()
+    .isEmpty(),
+  updatePost
+);
 router.delete('/:id', deletePost);
 
 export default router;
