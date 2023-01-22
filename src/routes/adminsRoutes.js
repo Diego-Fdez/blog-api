d1a33c1916f1e60382b207cf0bd19d5c3a248871
Diego-Fdez <dieghoxcr7@gmail.com>
2023-01-22 10:13:53 -0600

    add swagger to routes

--- src/routes/adminsRoutes.js
+++ src/routes/adminsRoutes.js
@@ -12,8 +12,96 @@ import {
 const router = express.Router();
 const cache = apicache.middleware;
 
+/**
+ * @openapi
+ * /api/v1/admins:
+ *   get:
+ *     tags:
+ *       - Admins
+ *     parameters:
+ *       - in:
+ *         name: mode
+ *         schema:
+ *           type: string
+ *         description: Get list of all users
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     $ref: "#/components/schemas/Admins"
+ *       5XX:
+ *         description: FAILED
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: "Some error message"
+ */
 router.get('/', cache('60 minutes'), getUsers);
 router.get('/:id', getOneUser);
+/**
+ * @openapi
+ * /api/v1/admins:
+ *   post:
+ *     tags:
+ *       - Admins
+ *     parameters:
+ *       - in: body
+ *         name: mode
+ *         schema:
+ *           type: object
+ *         description: Create a new user
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     $ref: "#/components/schemas/Admins"
+ *       5XX:
+ *         description: FAILED
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: "Some error message"
+ */
 router.post(
   '/',
   body('email', 'email is required').not().isEmpty(),
